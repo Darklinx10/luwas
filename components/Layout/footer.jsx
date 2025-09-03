81e244b1add8381aefe3cbb96fccd4f45dad570f
Lynxden10 <lynxden10@gmail.com>
2025-09-03 08:55:51 +0800

    2tf

--- components/Layout/footer.jsx
+++ components/Layout/footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer className="absolute bottom-4 min-w-screen flex justify-center">
       <p className="text-xs text-gray-400 text-center leading-relaxed">
-        
+        © {new Date().getFullYear()} <br />
         Bohol Island State University Clarin Campus <br />
-        MDRRMC of the Municipality of Clarin <br />
-        © {new Date().getFullYear()} 
+        MDRRMC of the Municipality of Clarin
+        
       </p>
     </footer>
   );
