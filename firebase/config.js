b0eec0b767644a8c2b2d6808a7f7118e40060cb5
Lynxden10 <lynxden10@gmail.com>
2025-09-23 18:09:36 +0800

    wtf

--- firebase/config.js
+++ firebase/config.js
@@ -1,7 +1,7 @@
-// Import firebase the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// lib/firebaseClient.js
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -13,10 +13,11 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Use existing app if already initialized (important for Next.js hot reload)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { auth, db, storage };
+export { app, auth, db, storage };
